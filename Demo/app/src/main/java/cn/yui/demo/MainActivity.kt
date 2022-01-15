129f2188ddcbac2eafb79720c692ed5cd867089d
dev.liao <562898419@qq.com>
2022-01-15 19:27:25 +0800

    java转kotlin

--- Demo/app/src/main/java/cn/yui/demo/MainActivity.kt
+++ Demo/app/src/main/java/cn/yui/demo/MainActivity.kt
@@ -1,14 +1,16 @@
-package cn.yui.demo;
+package cn.yui.demo
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+/**
+ * @Description: 主页面
+ * @Author: Yui Master
+ * @CreateDate: 2022/1/15 7:23 下午
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
 }

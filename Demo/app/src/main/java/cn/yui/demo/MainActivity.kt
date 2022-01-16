f46624d38d15cae6f74503958de41f13c0a025c5
dev.liao <562898419@qq.com>
2022-01-16 10:41:24 +0800

    重新启用kapt，暂时移除hilt规避kapt的编译错误

--- Demo/app/src/main/java/cn/yui/demo/MainActivity.kt
+++ Demo/app/src/main/java/cn/yui/demo/MainActivity.kt
@@ -2,14 +2,14 @@ package cn.yui.demo
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import dagger.hilt.android.AndroidEntryPoint
+//import dagger.hilt.android.AndroidEntryPoint
 
 /**
  * @Description: 主页面
  * @Author: Yui Master
  * @CreateDate: 2022/1/15 7:23 下午
  */
-@AndroidEntryPoint
+//@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- Demo/app/src/main/java/cn/yui/demo/NewApplication.kt
+++ Demo/app/src/main/java/cn/yui/demo/NewApplication.kt
@@ -1,15 +1,15 @@
 package cn.yui.demo
 
 import android.app.Application
-import dagger.hilt.android.AndroidEntryPoint
-import dagger.hilt.android.HiltAndroidApp
+//import dagger.hilt.android.AndroidEntryPoint
+//import dagger.hilt.android.HiltAndroidApp
 
 /**
  * @Description: java类作用描述
  * @Author: Yui Master
  * @CreateDate: 2022/1/15 7:44 下午
  */
-@HiltAndroidApp
+//@HiltAndroidApp
 class NewApplication : Application() {
     override fun onCreate() {
         super.onCreate()
